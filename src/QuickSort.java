c509df29c1c630e75e2e7819480b9bae77c37725
cuj <akow283@gmail.com>
2022-05-03 14:01:21 +0900

    quickSort 완

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -3,14 +3,19 @@ import java.util.Arrays;
 public class QuickSort {
 	public static void main(String[] args) {
 		int[] arr= {2,4,1,9,7,3,6,8,5};
-		partition(arr,0,arr.length-1);
+		quickSort(arr,0,arr.length-1);
 		System.out.println(Arrays.toString(arr));
 	}
 	static void quickSort(int[] arr, int left, int right) {
+		if(left>=right) return;
 		
+		//한번 진행
+		int pivotPosition= partition(arr, left, right); 
+		quickSort(arr, left, pivotPosition-1);//가운데값(pivot) 기준 왼쪽거 또 quickSort
+		quickSort(arr, pivotPosition+1, right);// pivot 기준 오른쪽 quickSort
 	} 
 	
-	static void partition(int[] arr,int left,int right) {
+	static int partition(int[] arr,int left,int right) {
 		int pivot= arr[right] ;  //오른쪽이 pivot  
 		int i=left-1; 
 		for(int j=left; j<right ; j++) {  //작은 값들은 왼쪽으로... (arr[i]값이랑 자리 변경
@@ -20,8 +25,11 @@ public class QuickSort {
 			}	
 		}
 		i++;
-		swap(arr,right, i );
+		swap(arr,i, right );
+		return i; //pivot 위치 return
 	}
+	
+	
 	static void swap(int[] arr, int i, int j) {
 		int temp=arr[j];
 		arr[j]=arr[i];
