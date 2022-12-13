b26d6402e67c57d90640825a83d9cda7385ae5fc
gks93 <gks930620@naver.com>
2022-12-13 13:12:34 +0900

    2512 1654 이분탐색은 이제 쉽당

--- src/BOJ1654.java
+++ src/BOJ1654.java
@@ -1,48 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BOJ1654 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M;
+    static int K, N;
     static int[] A;
 
     static void input() {
+        K = scan.nextInt();
         N = scan.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        A = new int[K + 1];
+        for (int i = 1; i <= K; i++) {
             A[i] = scan.nextInt();
         }
-        M = scan.nextInt();
     }
 
-    static boolean determination(int limit) {  //1원만 주면 되긴하지..  예산초과 false,    돈 줄수있으면 true
-        // TODO
+    static boolean determination(long len) {
+        // len 만큼의 길이로 랜선들을 잘랐을 때, N 개 만큼의 랜선을 얻을 수 있는가?
         int sum=0;
-        for(int i=1; i<=N ; i++){
-            sum +=   A[i]>limit ? limit : A[i];
+        for(int i=1 ; i<=K; i++){
+            sum+=A[i]/len;
         }
-        return sum> M ? false : true;
+        return sum<N ? false : true;
     }
 
-    static void pro() {
-        int max=0;
-        for(int i=1 ; i<=N ; i++){
-            max=Math.max(A[i],max);
-        }
 
-        int L=1, R=max, ans=0; //
+    // int로 해도 되는 범위인데 L+R했을 때  int 범위 초과함
+    static void pro() {
+        long L = 1, R = Integer.MAX_VALUE, ans = 0;
         // [L ... R] 범위 안에 정답이 존재한다!
         // 이분 탐색과 determination 문제를 이용해서 answer를 빠르게 구하자!
         while(L<=R){
-            int mid=(L+R)/2;
-            if(determination(mid)){ //줄 수 있으면 상한액 더 늘려.
+            long mid=(L+R)/2;    //랜선의 길이는 2^31-1   int 범위
+            if(determination(mid)){     // n개를 얻을 수 있다? 더 키워야지
                 L=mid+1;
                 ans=mid;
             }else{
-                R=mid-1;
+                R=mid-1;  //n개 못 얻었다.  더 작게
             }
+
         }
         System.out.println(ans);
     }
--- src/Yet2512.java
+++
@@ -1,2 +0,0 @@
-public class Yet2512 {
-}
