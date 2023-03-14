c74a6b256340305701f17fd777548247f1c57380
gks93 <gks930620@naver.com>
2023-03-14 19:50:24 +0900

    KAKAO 문제 조금씩

--- src/KAKAOMain.java
+++ src/KAKAOMain.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class KAKAOMain {
     public static void main(String[] args) {
         String[] queries = {"hot", "dot", "dog", "lot", "log", "cog"};
+        //
     }
 
 
