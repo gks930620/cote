6a5578301b5e6f6425d71ebc5fe54bcf22859287
gks930620 <gks930620@naver.com>
2023-05-16 14:11:16 +0900

    여행경로는 나주엥

--- src/programers/그래프탐색/여행경로.java
+++ src/programers/그래프탐색/여행경로.java
@@ -8,9 +8,12 @@ public class 여행경로 { //실패  다시해보자.
         String[][] tickets2= {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
 
         String[][] tickets3= {{"ICN", "SFO"},{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
-        List<String> solution = new 여행경로().solution(tickets3);
+        List<String> solution = new 여행경로().solution(tickets2);
         System.out.println(solution);
     }
+
+    //중복이 허용, + 알파벳순서로 갔을 때 모든 경로 가는 걸 보장하지 않음 => 도시가 남아있을 때는  다른 경로를 탐색하도록 구성해야지..
+    //문제 자체가 dfs로 풀도록 되어있는 문제인데..
     Map<String, List<String>> map = new HashMap<>();
     List<String> answer = new ArrayList<>();
     public List<String> solution(String[][] tickets) {
@@ -23,28 +26,32 @@ public class 여행경로 { //실패  다시해보자.
         for(Map.Entry<String ,List<String >> entry : map.entrySet()){
             entry.getValue().sort(null);
         }
-        bfs("ICN");
+
+        dfs("ICN");  //다 썻는지 확인을 어케하냐..  //모르겠다..
+        if(answer.size() < tickets.length+1  ){ //항공권 다 못 쓴건데..
+
+        }
+
 
 
         return  answer;
     }
 
-    public void bfs(String start) {
-        PriorityQueue<String> que = new PriorityQueue<>();
-        que.add(start);
-        while (!que.isEmpty()){
-            String x= que.poll();
-            answer.add(x);
-            List<String> ends = map.get(x);
-            if(ends==null || ends.isEmpty()) continue;  // 최종목저지
-            for(int i=0 ; i<ends.size()  ; i++){  //알파벳순으로 정렬되어있음
+    public void dfs(String x ){
+        //x에 온걸로 간주
+        answer.add(x);
 
-                que.add(ends.get(i));
-                ends.remove(i);   // ICN=> ATL,SFO 중에서 ATL 갔다면 ATL 지우기  .  ICN에서 다시 출발해도 이번엔 ATL이 없음
-                i--;
-            }
+        //for   x에서 갈 수 있는 모든 점   정렬되어있음..
+        List<String> list = map.get(x);
+        if(list==null)  return; //최종목적지
+        for(int i=0 ; i<list.size() ; i++){
+            String y= list.get(i);  //다음 목저지
+            list.remove(i--);  //다음 목적지로 간다면   항공권 쓴거
+            dfs(y);  //다음 목적지로 간다.
         }
     }
+
+
 }
 
 
