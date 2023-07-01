57f3665d678b01f16919012721ef8a1c16347bcd
gks930620 <gks930620@naver.com>
2023-07-01 22:47:33 +0900

    최단경로

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,102 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int n;
-    static int[][] childs;
+    static class Edge {
+        public int to, weight;
+
+        public Edge(int _to, int _weight) {
+            this.to = _to;
+            this.weight = _weight;
+        }
+    }
+
+
+    //왜 info가 필요할까?..   Edge만으론 안됬나?
+    static class Info {
+        public int idx, dist;
+
+        public Info() {
+        }
+
+        public Info(int _idx, int _dist) {
+            this.idx = _idx;
+            this.dist = _dist;
+        }
+    }
+
+    static int N, M, K;
+    static int[] dist;
+    static ArrayList<Edge>[] edges;
 
     static void input() {
-        /* TODO */
+        N = scan.nextInt();
+        M = scan.nextInt();
+        K=scan.nextInt();
+        dist = new int[N + 1];   //start부터 N까지의 거리
+        edges = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) edges[i] = new ArrayList<Edge>();
+        for (int i = 1; i <= M; i++) {
+            int from = scan.nextInt();
+            int to = scan.nextInt();
+            int weight = scan.nextInt();
+            edges[from].add(new Edge(to, weight));
+        }
     }
 
-    static void in_order(int x) {
+    static void dijkstra(int start) {
+        // 모든 정점까지에 대한 거리를 무한대로 초기화 해주기.
+        // ※주의사항※
+        // 문제의 정답으로 가능한 거리의 최댓값보다 큰 값임을 보장해야 한다.
         /* TODO */
-    }
+        for(int i=1 ; i<=N ; i++){
+            dist[i]=Integer.MAX_VALUE;
+        }
+        // 최소 힙 생성
+        /* TODO */
+        PriorityQueue<Info> pq=new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist); //start에서의 거리가 짧은게 먼저나옴
 
-    static void pre_order(int x) {
+        // 시작점에 대한 정보(Information)을 기록에 추가하고, 거리 배열(dist)에 갱신해준다.
         /* TODO */
-    }
+        pq.add(new Info(start,0 ));
+        dist[start]=0;
+        // 거리 정보들이 모두 소진될 때까지 거리 갱신을 반복한다.
+        while (!pq.isEmpty()) {
+            Info info = pq.poll();
 
-    static void post_order(int x) {
-        /* TODO */
+            // 꺼낸 정보가 최신 정보랑 다르면, 의미없이 낡은 정보이므로 폐기한다.
+            /* TODO */
+            if(info.dist > dist[info.idx])continue;
+
+            // 연결된 모든 간선들을 통해서 다른 정점들에 대한 정보를 갱신해준다.
+            for (Edge e : edges[info.idx]) {
+                // e.to 까지 갈 수 있는 더 짧은 거리를 찾았다면 이에 대한 정보를 갱신하고 PQ에 기록해준다.
+                /* TODO */
+
+                if( dist[e.to] > dist[info.idx] + e.weight ){
+                    pq.add(new Info(e.to , dist[info.idx]+e.weight ));
+                    dist[e.to]=dist[info.idx]+e.weight;
+                }
+
+
+            }
+        }
     }
 
     static void pro() {
+        dijkstra(K);
         /* TODO */
+        for(int i=1 ; i<=N ; i++){
+            if(dist[i]==Integer.MAX_VALUE){
+                sb.append("INF").append("\n");
+            }else{
+                sb.append(dist[i]).append("\n");
+            }
+        }
+        System.out.print(sb);
     }
 
     public static void main(String[] args) {
